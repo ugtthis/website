20bc7235f0579e03f0a5ceac6b90409f8f67c390
Cameron Clough <cameronjclough@gmail.com>
2025-03-26 11:55:30 +0000

    cleanup: compatibility script template paths (#60)
    cleanup compatibility script

--- scripts/compatibility.py
+++ scripts/compatibility.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-import os
+from pathlib import Path
 from opendbc.car.docs import generate_cars_md, get_all_car_docs
 
-WEB4_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
-VEHICLES_OUT_FILE = os.path.join(WEB4_DIR, "src/lib/vehicles.json")
-HARNESS_PARTS_OUT_FILE = os.path.join(WEB4_DIR, "static/harness-parts.html")
+WEB4_DIR = Path(__file__).parent.parent
+TEMPLATES = ((WEB4_DIR / "templates/vehicles_template.json", WEB4_DIR / "src/lib/vehicles.json"),
+             (WEB4_DIR / "templates/harness-parts_template.html", WEB4_DIR / "static/harness-parts.html"))
 
-if __name__ == '__main__':
-  for out_file, template_file in ((VEHICLES_OUT_FILE, "templates/vehicles_template.json"),
-                                  (HARNESS_PARTS_OUT_FILE, "templates/harness-parts_template.html")):
-    content = generate_cars_md(get_all_car_docs(), os.path.join(WEB4_DIR, template_file))
-    with open(out_file, "w") as f:
-      f.write(content)
-    print(f"Generated and written to {out_file}")
+if __name__ == "__main__":
+  for template_path, out_path in TEMPLATES:
+    content = generate_cars_md(get_all_car_docs(), template_path)
+    out_path.write_text(content)
+    print(f"Generated and written to {out_path}")
