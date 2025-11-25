815870ae2b0de747f2c2b61d049a4fbc463bf051
Shane Smiskol <shane@smiskol.com>
2025-11-25 02:23:55 -0800

    Non-breaking hyphen in no harness discount (#243)

--- src/lib/constants/vehicles.js
+++ src/lib/constants/vehicles.js
@@ -3,4 +3,4 @@ import vehicles from '$lib/vehicles.json'
 export const vehicleCountText = `${Math.round(Object.values(vehicles).reduce((sum, list) => sum + list.length, 0) / 25) * 25}+`;
 
 // Harness selection options
-export const NO_HARNESS_OPTION = { car: "I already have a car harness (-$50)" };
+export const NO_HARNESS_OPTION = { car: "I already have a car harness (‑$50)" };
